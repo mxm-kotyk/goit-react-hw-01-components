f9d6f130ce2c5675c1ee83411c8a33adabfc9616
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-05-03 22:04:48 +0200

    Refractored FriendsList components

--- src/components/FriendList/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList/FriendList.jsx
@@ -1,8 +1,17 @@
 import { List } from './FriendList.styled'
 import { FriendListItem } from '../FriendListItem'
+import PropTypes from 'prop-types'
+
 
 export const FriendList = ({friends}) => {
   return (<List>
-    <FriendListItem friends={friends}/>
+    {friends.map(friend => { return (<FriendListItem friend={friend} key={friend.id}/>) })}
+    
   </List>)
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired
+  }))
 }
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,19 +1,19 @@
 import { Item, Name, Status } from './FriendListItem.styled'
 import PropTypes from 'prop-types'
 
-export const FriendListItem = ({ friends }) => {
-  return (friends.map(({avatar, name, isOnline, id}) => {return <Item key={id}>
-  <Status available={isOnline}></Status>
-  <img src={avatar} alt="User avatar" width="48" />
-    <Name>{name}</Name>
-</Item>}))
+export const FriendListItem = ({ friend }) => {
+  const {avatar, name, isOnline} = friend
+  return (<Item>
+    <Status available={isOnline}></Status>
+    <img src={avatar} alt="User avatar" width="48" />
+      <Name>{name}</Name>
+  </Item>)
 }
 
 FriendListItem.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.exact({
+  friend: PropTypes.shape({
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number.isRequired
-  }))
+  })
 }
